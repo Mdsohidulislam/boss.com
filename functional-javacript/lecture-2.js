61a6c5b35df290f4957662b5bd0e6bd97fec432b
Mdsohidulislam <mddsohidulislam@gmail.com>
2020-12-13 07:06:56 +0600

    boss.com uploading for future moments

---
+++ functional-javacript/lecture-2.js
@@ -0,0 +1,11 @@
+function hello(name){
+    console.log(name);
+    print(name)
+    print('Print somethings else')
+}
+
+function print(para1){
+    console.log(para1);
+}
+
+hello('md sohidul islam')
